be7412eb05e87fb6b4e059c6543615765d145a5e
gursandeep <gursandeep.singh@gmail.com>
2025-02-09 16:40:37 +1100

    End of section 2

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,34 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options => 
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+  DbInitializer.InitiDb(app);
+}
+catch (Exception ex)
+{
+  Console.WriteLine(ex.Message);
+}
+
+app.Run();
